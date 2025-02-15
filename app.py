8563e09661671e9f0649d9a832a0c68116475025
Armanlaliwala <110816459+Armanlaliwala@users.noreply.github.com>
2025-02-16 01:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def generate_qr():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=10000)
